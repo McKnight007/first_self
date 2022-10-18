6174e79cf4e47af590a399c48ac873f29a3078bf
McKnight007 <114586694+McKnight007@users.noreply.github.com>
2022-10-19 01:56:43 +0400

    Second commit

--- lb_to_kg.py
+++ lb_to_kg.py
@@ -11,3 +11,8 @@ var_kgs = float(var_pounds*TO_KGS)
 # Rounding off the value to 2 decimal places
 # Change it according to your preference
 print("That is", var_kgs.__round__(2), "kilograms")
+
+# A cleaner way of rounding off
+print("That is", round(var_kgs, 2), "kilograms")
+
+
