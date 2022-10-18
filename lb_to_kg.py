dd3f20a30dbdf99a064634085507696be783779c
McKnight007 <114586694+McKnight007@users.noreply.github.com>
2022-10-19 01:47:21 +0400

    First commit

---
+++ lb_to_kg.py
@@ -0,0 +1,13 @@
+# Creating constants
+TO_KGS = 0.453592
+
+# Assigning input values to a variable
+var_pounds = float(input("Type the value in pounds: "))
+
+# function for changing kgs to pounds
+
+var_kgs = float(var_pounds*TO_KGS)
+
+# Rounding off the value to 2 decimal places
+# Change it according to your preference
+print("That is", var_kgs.__round__(2), "kilograms")
